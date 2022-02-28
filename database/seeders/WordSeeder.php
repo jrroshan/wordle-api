9ecd139415110152134e1bb5d95fc9bcd0c349ba
Roshan Lasiwa <roshanlasiwa@gmail.com>
2022-02-28 16:52:35 +0545

    added words

--- database/seeders/WordSeeder.php
+++ database/seeders/WordSeeder.php
@@ -17,12 +17,15 @@ class WordSeeder extends Seeder
     public function run()
     {
         // $datas =
-        $words = explode(" ", file_get_contents("words.txt"));
+        $words = explode(" ", file_get_contents('words.txt'));
+        shuffle($words);
         $time = 6;
         foreach($words as $word){
+            if(strlen($word) == 5 && ctype_alpha($word)){
             $data = ['words'=>$word,'dateTime'=>Carbon::now()->addHour($time)];
             Word::create($data);
             $time+=6;
+        }
         }
 
     }
